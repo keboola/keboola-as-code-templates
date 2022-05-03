02e4d6aadbee211b927be4d11dcc9a920d304e11
Monika Feigler <monika@feigler.cz>
2022-05-03 14:08:45 +0200

    fix columns

--- _common/in-ecommerce-shopify/v0/src/transformation/keboola.snowflake-transformation/in-ecommerce-shopify-transformation2-data-preparation-10697799/blocks/001-gather-data/003-products/code.sql
+++ _common/in-ecommerce-shopify/v0/src/transformation/keboola.snowflake-transformation/in-ecommerce-shopify-transformation2-data-preparation-10697799/blocks/001-gather-data/003-products/code.sql
@@ -1,70 +1,110 @@
 --BDM_PRODUCTS
-CREATE OR REPLACE TABLE "bdm_products" 
-AS
-     SELECT 
-          P."id"                                                                               AS PRODUCT_ID
-          , PV."id"                                                                            AS PRODUCT_CODE
-          , ''::STRING                                                                         AS PRODUCT_CODE_2
-          , ''::STRING                                                                         AS PRODUCT_GUID
-          , P."title"                                                                          AS PRODUCT_NAME
-          , P."vendor"                                                                         AS PRODUCT_MANUFACTURER
-          , P."product_type"                                                                   AS PRODUCT_TYPE
-          , PV."barcode"                                                                       AS PRODUCT_EAN
-          , REPLACE(NULLIF(PV."price", ''), ',', '.')::DECIMAL(19, 2)                          AS PRODUCT_PRICE
-          , NULL                                                                               AS PRODUCT_STANDARD_PRICE
-          , NULL                                                                               AS PRODUCT_PURCHASE_PRICE
-          , PV."option1"                                     						     AS PRODUCT_OPTION1
-          , PV."option1"                                      							AS PRODUCT_OPTION2
-          , PV."option3"														     AS PRODUCT_OPTION3
-          , PV."inventory_quantity"                                                            AS PRODUCT_STOCK_AMOUNT
-          , ''::string                                                                         AS CATEGORY
-          , ''::string                                                                         AS CATEGORY_TEXT
-          , 'insert www page with products' || P."handle"                                      AS PRODUCT_URL
-          , 'false'                                                                            AS IS_DELETED
-     FROM "product_variant" PV
-          LEFT JOIN "product" P 
-               ON P."id"=PV."product_id"
-;
+
+CREATE TABLE "bdm_products" AS
+SELECT
+    P."code"                                                                                     AS PRODUCT_ID
+  , P."code"                                                                                     AS PRODUCT_CODE
+  , P."pairCode"                                                                                 AS PRODUCT_CODE_2
+  , P."guid"                                                                                     AS PRODUCT_GUID
+  , P."name"                                                                                     AS PRODUCT_NAME
+  , P."manufacturer"                                                                             AS PRODUCT_MANUFACTURER
+  , P."itemType"                                                                                 AS PRODUCT_TYPE
+  , P."ean"                                                                                      AS PRODUCT_EAN
+  , REPLACE(NULLIF(P."price", ''), ',', '.')::DECIMAL(19, 2)                                     AS PRODUCT_PRICE
+  , REPLACE(NULLIF(P."standardPrice", ''), ',', '.')::DECIMAL(19, 2)                             AS PRODUCT_STANDARD_PRICE
+  , REPLACE(NULLIF(P."purchasePrice", ''), ',', '.')::DECIMAL(19, 2)                             AS PRODUCT_PURCHASE_PRICE
+  , P."variantBarva"                                                                             AS PRODUCT_OPTION1
+  , P."variantVelikost"                                                                          AS PRODUCT_OPTION2
+  , 'Insert another varinat for product option'                                                  AS PRODUCT_OPTION3
+  , S."stock"                                                                                    AS PRODUCT_STOCK_AMOUNT
+  , P."defaultCategory"                                                                          AS CATEGORY
+  , P."categoryText"                                                                             AS CATEGORY_TEXT
+  , (SELECT "shop_base_url" FROM "shoptet" LIMIT 1) || '/admin/produkty-detail/?id=' || P."code" AS PRODUCT_URL
+  , 'false'                                                                                      AS IS_DELETED
+FROM
+    "products" P
+        LEFT JOIN "stocks" S
+                  ON P."code" = S."itemCode";
+
+--- ADD artificial
+INSERT INTO "bdm_products"
+SELECT 'BILLING'      AS PRODUCT_ID
+     , 'BILLING'      AS PRODUCT_CODE
+     , 'N/A'          AS PRODUCT_CODE_2
+     , 'N/A'          AS PRODUCT_GUID
+     , 'billing item' AS PRODUCT_NAME
+     , 'N/A'          AS PRODUCT_MANUFACTURER
+     , 'billing'      AS PRODUCT_TYPE
+     , 'N/A'          AS PRODUCT_EAN
+     , NULL           AS PRODUCT_PRICE
+     , NULL           AS PRODUCT_STANDARD_PRICE
+     , NULL           AS PRODUCT_PURCHASE_PRICE
+     , 'N/A'          AS PRODUCT_OPTION1
+     , 'N/A'          AS PRODUCT_OPTION2
+     , 'N/A'          AS PRODUCT_OPTION3
+     , NULL           AS PRODUCT_STOCK_AMOUNT
+     , 'N/A'          AS CATEGORY
+     , 'N/A'          AS CATEGORY_TEXT
+     , 'N/A'          AS PRODUCT_URL
+     , 'false'        AS IS_DELETED;
+
+INSERT INTO "bdm_products"
+SELECT 'SHIPPING'      AS PRODUCT_ID
+     , 'SHIPPING'      AS PRODUCT_CODE
+     , 'N/A'           AS PRODUCT_CODE_2
+     , 'N/A'           AS PRODUCT_GUID
+     , 'shipping item' AS PRODUCT_NAME
+     , 'N/A'           AS PRODUCT_MANUFACTURER
+     , 'shipping'      AS PRODUCT_TYPE
+     , 'N/A'           AS PRODUCT_EAN
+     , NULL            AS PRODUCT_PRICE
+     , NULL            AS PRODUCT_STANDARD_PRICE
+     , NULL            AS PRODUCT_PURCHASE_PRICE
+     , 'N/A'          AS PRODUCT_OPTION1
+     , 'N/A'          AS PRODUCT_OPTION2
+     , 'N/A'          AS PRODUCT_OPTION3
+     , NULL            AS PRODUCT_STOCK_AMOUNT
+     , 'N/A'           AS CATEGORY
+     , 'N/A'           AS CATEGORY_TEXT
+     , 'N/A'           AS PRODUCT_URL
+     , 'false'         AS IS_DELETED;
 
 --- ADD missing / deleted items
 
-CREATE OR REPLACE TABLE "deleted_products" 
-AS
-     SELECT DISTINCT 
-          "product_id"               AS PRODUCT_ID
-          , ''::STRING               AS PRODUCT_TYPE
-          , ANY_VALUE("name")        AS PRODUCT_NAME
-     FROM "line_item" O
-         JOIN "product" P
-               ON O."product_id" = P."id"
-     WHERE P."id" IS NULL
-     GROUP BY 1, 2
+CREATE TABLE "deleted_products" AS
+SELECT DISTINCT "itemCode"            AS PRODUCT_ID
+              , "orderItemType"       AS PRODUCT_TYPE
+              , ANY_VALUE("itemName") AS PRODUCT_NAME
+FROM "orders" O
+         LEFT JOIN "products" P
+                   ON O."itemCode" = P."code"
+WHERE O."orderItemType" NOT IN ('shipping', 'billing')
+  AND P."code" IS NULL
+GROUP BY 1, 2
 ;
 
 INSERT INTO "bdm_products"
-     SELECT 
-          DP.PRODUCT_ID     AS PRODUCT_ID
-          , DP.PRODUCT_ID   AS PRODUCT_CODE
-          , 'N/A'           AS PRODUCT_CODE_2
-          , 'N/A'           AS PRODUCT_GUID
-          , DP.PRODUCT_NAME AS PRODUCT_NAME
-          , 'N/A'           AS PRODUCT_MANUFACTURER
-          , DP.PRODUCT_TYPE AS PRODUCT_TYPE
-          , 'N/A'           AS PRODUCT_EAN
-          , NULL            AS PRODUCT_PRICE
-          , NULL            AS PRODUCT_STANDARD_PRICE
-          , NULL            AS PRODUCT_PURCHASE_PRICE
-          , 'N/A'           AS PRODUCT_OPTION1
-          , 'N/A'           AS PRODUCT_OPTION2
-          , 'N/A'           AS PRODUCT_OPTION3
-          , NULL            AS PRODUCT_STOCK_AMOUNT
-          , 'N/A'           AS CATEGORY
-          , 'N/A'           AS CATEGORY_TEXT
-          , 'N/A'           AS PRODUCT_URL
-          , 'true'          AS IS_DELETED
-     FROM "deleted_products" DP
-     QUALIFY ROW_NUMBER() OVER (PARTITION BY DP.PRODUCT_ID ORDER BY DP.PRODUCT_TYPE) = 1
-;
+SELECT DP.PRODUCT_ID   AS PRODUCT_ID
+     , DP.PRODUCT_ID   AS PRODUCT_CODE
+     , 'N/A'           AS PRODUCT_CODE_2
+     , 'N/A'           AS PRODUCT_GUID
+     , DP.PRODUCT_NAME AS PRODUCT_NAME
+     , 'N/A'           AS PRODUCT_MANUFACTURER
+     , DP.PRODUCT_TYPE AS PRODUCT_TYPE
+     , 'N/A'           AS PRODUCT_EAN
+     , NULL            AS PRODUCT_PRICE
+     , NULL            AS PRODUCT_STANDARD_PRICE
+     , NULL            AS PRODUCT_PURCHASE_PRICE
+     , 'N/A'          AS PRODUCT_OPTION1
+     , 'N/A'          AS PRODUCT_OPTION2
+     , 'N/A'          AS PRODUCT_OPTION3
+     , NULL            AS PRODUCT_STOCK_AMOUNT
+     , 'N/A'           AS CATEGORY
+     , 'N/A'           AS CATEGORY_TEXT
+     , 'N/A'           AS PRODUCT_URL
+     , 'true'          AS IS_DELETED
+FROM "deleted_products" DP
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY DP.PRODUCT_ID ORDER BY DP.PRODUCT_TYPE) = 1;
 
 -- some do not have code
 UPDATE "bdm_products"
