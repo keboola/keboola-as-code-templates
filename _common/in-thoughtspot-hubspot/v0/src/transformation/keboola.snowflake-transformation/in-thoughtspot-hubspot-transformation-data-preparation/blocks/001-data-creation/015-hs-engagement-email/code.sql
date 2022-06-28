b371582d72e4780e14d0ec7dff3a848e0cb0b68f
Monika Feigler <monika@feigler.cz>
2022-06-28 14:55:31 +0200

    thoughtspothubspot

---
+++ _common/in-thoughtspot-hubspot/v0/src/transformation/keboola.snowflake-transformation/in-thoughtspot-hubspot-transformation-data-preparation/blocks/001-data-creation/015-hs-engagement-email/code.sql
@@ -0,0 +1,22 @@
+-- HS_HUBSPOT."PUBLIC".HS_ENGAGEMENT_EMAIL
+CREATE TABLE HS_ENGAGEMENT_EMAIL AS
+SELECT DISTINCT
+-- "html" 
+''AS HTML,
+-- "engagement_id" 
+'' AS ENGAGEMENT_ID,
+-- "from_email" 
+''AS FROM_EMAIL,
+--"sender_email" 
+'' AS SENDER_EMAIL,
+-- "subject" 
+'' AS SUBJECT,
+-- "text" 
+''AS TEXT,
+-- "status" 
+''AS STATUS,
+-- "from_first_name" 
+''AS FROM_FIRST_NAME,
+-- "from_last_name" 
+''AS FROM_LAST_NAME
+FROM "emails";
