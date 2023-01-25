15fb0e4fb55bad77bbd1cc1022afff45e7f276f8
Monika Feigler <monika@feigler.cz>
2023-01-25 10:44:10 +0100

    release dtaa quality

---
+++ data-quality/v0/src/description.md
@@ -0,0 +1,3 @@
+We've prepared shared codes which can help you with data quality checks. 
+Use this template and check how those codes can be used in your transformations.
+
---
+++ data-quality/v0/src/transformation/keboola.snowflake-transformation/data-quality-core-full-example/blocks/001-block-1/028-test-column-value-in-set/code.sql
@@ -0,0 +1,8 @@
+// Tests whether all values in given column in given table are in provided set of values.Fails when value is not in provided set of values.
+call TEST_VALUE_IN_SET('orders', 'product', '(\'Rubber\',\'Hat Won\')');
+
+// Store results in table DQ_RESULTS_LOG.  [WARNING/FAIL]
+call LOG_PREVIOUS_TEST_RESULT('WARNING');
+
+// Abort if any FAIL  runs
+SET ABORT_TRANSFORMATION = (SELECT GET_LATEST_FAILED_MESSAGE());
