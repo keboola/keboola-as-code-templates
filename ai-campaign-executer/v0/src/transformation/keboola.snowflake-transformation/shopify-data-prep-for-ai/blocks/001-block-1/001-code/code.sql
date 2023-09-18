9fee5d3a6f106176656fef60fd50de96ab7c04a6
Monika Feigler <monika@feigler.cz>
2023-09-18 15:40:20 +0200

    fix

--- ai-campaign-executer/v0/src/transformation/keboola.snowflake-transformation/shopify-data-prep-for-ai/blocks/001-block-1/001-code/code.sql
+++ ai-campaign-executer/v0/src/transformation/keboola.snowflake-transformation/shopify-data-prep-for-ai/blocks/001-block-1/001-code/code.sql
@@ -2,7 +2,7 @@ CREATE TABLE "out_sms_campaign" AS
 SELECT
     o."customer_id",
     c."first_name" AS "name",
-    '+420123456789' AS "phone",
+    c."phone" AS "phone",
     count(li."product_id") AS "count",
     p."title",
     p."body_html"
@@ -11,4 +11,9 @@ JOIN "customer" c ON c."id" = o."customer_id"
 JOIN "line-item" li ON li."order_id" = o."id"
 JOIN "product" p ON p."id" = li."product_id"
 WHERE o."customer_id" != ''
-GROUP BY 1,2,5,6;
+GROUP BY 
+	o."customer_id",
+	c."first_name",
+  c."phone",
+  p."title",
+  p."body_html";
