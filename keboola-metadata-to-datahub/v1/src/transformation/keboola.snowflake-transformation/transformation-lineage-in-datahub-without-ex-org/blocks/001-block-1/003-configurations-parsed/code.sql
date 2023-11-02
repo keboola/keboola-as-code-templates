aa8fd451bb79362cf3899040d823c845e8ca9a80
Monika Feigler <monika@feigler.cz>
2023-11-02 15:01:21 +0100

    datathub update

--- keboola-metadata-to-datahub/v1/src/transformation/keboola.snowflake-transformation/transformation-lineage-in-datahub-without-ex-org/blocks/001-block-1/003-configurations-parsed/code.sql
+++ keboola-metadata-to-datahub/v1/src/transformation/keboola.snowflake-transformation/transformation-lineage-in-datahub-without-ex-org/blocks/001-block-1/003-configurations-parsed/code.sql
@@ -1,11 +1,12 @@
-create or replace table "configurations_parsed" as
-select c.*,
-    p."kbc_project" as "project_name"
-from (
-        select *
-        from "rows_json" c
-        union all
-        select *
-        from "configuration_json" c
-    ) as c
-    left join "kbc_project" p ON p."kbc_project_id_num" = c."project_id";
+CREATE TABLE "configurations_parsed" AS
+SELECT 
+  *
+FROM 
+  "rows_json" c 
+ 
+UNION ALL
+
+SELECT
+  *
+FROM 
+  "configuration_json" c;
