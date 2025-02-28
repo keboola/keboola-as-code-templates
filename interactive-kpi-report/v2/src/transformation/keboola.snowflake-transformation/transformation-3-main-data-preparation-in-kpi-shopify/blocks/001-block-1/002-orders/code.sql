59639c32f94d032d17ec01551aef73d3e2c3fbd5
Monika Feigler <monika@feigler.cz>
2025-02-28 14:35:10 +0100

    Interactive KPI report - correction of BigQuery SQL

--- interactive-kpi-report/v2/src/transformation/keboola.snowflake-transformation/transformation-3-main-data-preparation-in-kpi-shopify/blocks/001-block-1/002-orders/code.sql
+++ interactive-kpi-report/v2/src/transformation/keboola.snowflake-transformation/transformation-3-main-data-preparation-in-kpi-shopify/blocks/001-block-1/002-orders/code.sql
@@ -1,21 +1,20 @@
-CREATE TABLE "orders" AS
-WITH orders AS
-(
+CREATE TABLE `orders` AS
+WITH orders AS (
   SELECT
-  "updated_at"::date AS "date"
-  ,'Orders' AS "kpi_name"
-  ,count(*) AS "actual_value"
-  ,'number' AS "type"
-  FROM "order" 
-  --where "financial_status"='paid'
-  GROUP BY "updated_at"::date
+  	DATE(TIMESTAMP(`updated_at`))AS `date`,
+    'Orders' AS `kpi_name`,
+    COUNT(*) AS `actual_value`,
+    'number' AS `type`
+  FROM `order`
+  GROUP BY
+    DATE(TIMESTAMP(`updated_at`))
 )
-SELECT 
-o."date"
-,o."kpi_name"
-,o."actual_value"
-,p."plan_value"
-,o."type"
-FROM orders o
-LEFT JOIN "plan_daily" p ON o."kpi_name" = p."metric_name" AND o."date" = p."date"
-;
+SELECT
+  o.`date`,
+  o.`kpi_name`,
+  o.`actual_value`,
+  p.`plan_value`,
+  o.`type`
+FROM orders AS o
+LEFT JOIN `plan_daily` AS p
+    ON o.`kpi_name` = p.`metric_name` AND o.`date` = CAST(p.`date` AS DATE);
--- interactive-kpi-report/v2/src/transformation/keboola.snowflake-transformation/transformation-3-main-data-preparation-in-kpi-shopify/blocks/001-block-1/003-new-customers/code.sql
+++ interactive-kpi-report/v2/src/transformation/keboola.snowflake-transformation/transformation-3-main-data-preparation-in-kpi-shopify/blocks/001-block-1/003-new-customers/code.sql
@@ -1,21 +1,22 @@
-CREATE TABLE "new_customers" AS 
-WITH new_customers AS
-(
-  SELECT 
-  "updated_at"::date AS "date"
-  ,'New Customers' AS "kpi_name"
-  ,count(*) AS "actual_value"
-  ,'number' AS "type"
-  FROM "customer" 
-  WHERE "last_order_id"=''
-  GROUP BY "updated_at"::date
+CREATE TABLE `new_customers` AS
+WITH new_customers AS (
+  SELECT
+ 		DATE(TIMESTAMP(`updated_at`)) AS `date`,
+    'New Customers' AS `kpi_name`,
+    COUNT(*) AS `actual_value`,
+    'number' AS `type`
+  FROM `customer`
+  WHERE
+    `last_order_id` = ''
+  GROUP BY
+    DATE(TIMESTAMP(`updated_at`))
 )
-SELECT 
-  nc."date"
-  ,nc."kpi_name"
-  ,nc."actual_value"
-  ,p."plan_value"
-  ,nc."type"
-FROM new_customers nc
-LEFT JOIN "plan_daily" p ON nc."kpi_name" = p."metric_name" AND nc."date" = p."date"
-;
+SELECT
+  nc.`date`,
+  nc.`kpi_name`,
+  nc.`actual_value`,
+  p.`plan_value`,
+  nc.`type`
+FROM new_customers AS nc
+LEFT JOIN `plan_daily` AS p
+  ON nc.`kpi_name` = p.`metric_name` AND nc.`date` = CAST(p.`date` AS DATE);
